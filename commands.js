6965ab5e2f5e431507c82cd9df0816194c0c19ce
mattcallinan <peepthenudes@gmail.com>
2020-03-16 02:36:09 -0500

    added old beans

--- commands.js
+++ commands.js
@@ -3,6 +3,7 @@ const lukeisbeans = require('./methods/lukeisbeans.js')
 const mapleCharacter = require('./methods/mapleCharacter.js')
 const hpWashing = require('./methods/hpWashing/hpWashing.js')
 const invite = require('./methods/invite.js')
+const mattsnewfile =require('./methods/matt.js')
 
 const command = (msgContent) => {
   switch(msgContent.args[0]) {
@@ -12,6 +13,7 @@ const command = (msgContent) => {
     case "washes"        : return hpWashing.main(msgContent)
     case "link"          :
     case "invite"        : return invite.main(msgContent)
+    case "mattscommand"  : return mattsnewfile.main(msgContent)
     default              : return null
   }
 }
